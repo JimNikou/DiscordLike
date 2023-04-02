1c33d51323013ac7789ae18b735d1bdcabbef0f6
JimNikou <119452959+JimNikou@users.noreply.github.com>
2023-04-02 19:27:19 +0300

    home_screen_50%

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const Notifications = require('./js/notifications.js');
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 820,
-    height: 550,
+    width: 1280,
+    height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'js/preload.js'),
     },
   });
-
+  win.setMinimumSize(920, 590);
   win.loadFile('index.html');
 };
 
